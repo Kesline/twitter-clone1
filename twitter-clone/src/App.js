18d1863bdb89fbac6da763b5c46b1876dcdc41d3
Kesline Diogene <deka@Keslines-MacBook-Air.local>
2023-12-26 16:54:47 -0500

    first commit

---
+++ twitter-clone/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './App.css';
+
+function App() {
+  return (
+    //BEM
+    <div className="app">
+      <h1>Hey Clever Programmers...Let's build a Twitter Clone🚀</h1>
+      {/* Sidebar */}
+
+
+      {/* Feed */}
+
+      {/* Widgets*/}
+    </div>
+  );
+}
+
+export default App;
---
+++ twitter-clone/src/Sidebar.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Sidebar(){
+    return(
+        <div className="sidebar">
+            {/* Twitter Icon */}
+
+            {/*SidebarOption */}
+            {/*SidebarOption */}
+            {/*SidebarOption */}
+            {/*SidebarOption */}
+            {/*SidebarOption */}
+            {/*SidebarOption */}
+            {/*SidebarOption */}
+            
+            {/* Button --> Tweet */}
+        </div>
+    );
+}
+
+export default Sidebar;
